30cb8f0d00b501ac41b1b661abf9329ed1fe521a
Zayd Alzein <zaydalzein@gmail.com>
2022-03-27 00:12:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,38 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
 import time
+import csv
+import colorama
+from colorama import Fore, Back, init 
+init(autoreset=True)
 
 league_gf = 3.092
+
+
+def read_csv():
+    #Date,Visitor,G,Home,G,,Att.,LOG,Notes
+    with open('data.csv', 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        data = list(reader)
+        data.remove(data[0])
+    return data
+
+def getTeamRecords(team):
+    data=read_csv()
+    goalsFor = 0
+    goalsAgainst = 0
+    print(data[0])
+    for i in range(len(data)):
+        if team==data[i][1]:
+            goalsFor = goalsFor+int(data[i][2])
+            goalsAgainst = goalsAgainst+int(data[i][4])
+        elif team==data[i][3]:
+            goalsAgainst = goalsFor+int(data[i][2])
+            goalsFor= goalsAgainst+int(data[i][4])
+    print(goalsFor,goalsAgainst)
+        
+getTeamRecords('Boston Bruins')
+
 
 class matchup:
     def __init__(self,home,away) -> None: #param type: tuple (name,gf,ga,attack strength, defense strength)
@@ -21,6 +51,7 @@ class matchup:
         poisson_away=[]
         home_odds=0
         away_odds=0
+        tie_odds=0
         column=0
         row=0
         outof=10
@@ -36,10 +67,18 @@ class matchup:
                     home_odds=home_odds+(chance*chance2)
                 elif column>row:
                     away_odds=away_odds+(chance*chance2)
+                else: 
+                    tie_odds=tie_odds+(chance*chance2)
+
                 print(column,row,'|',chance*chance2)
                 row+=1
             column+=1
             row=0
-        print(((1-away_odds)*100),away_odds*100)
+        print(Back.RED+"Real Odds")
+        print(self.home[0]+" : "+str(round(home_odds*100,3))+"%")
+        print(self.away[0]+' : '+str(round(away_odds*100,3))+'%')
+        print("Tie Odds: "+str(round(tie_odds*100,3))+'%')
+        print('-----------------------------------------------------')
+        print((1-away_odds)*100,away_odds*100)
 
-matchup(("Vegas Golden Knights",3,2.83,0.970,0.986),("Nashville Predators",3.3,2.61,1.067,0.910)).poisson_calculator()
+matchup(("Calgary Flames",3.4,2.29,1.100,0.798),("Edmonton Oilers",3.3,3.06,1.067,1.067)).poisson_calculator()
